d0a93ffdb04b33c0392a200daf422b31671c41df
Solra Bizna <solra@bizna.name>
2022-02-21 14:10:50 -0700

    Start writing documentation and then stop for no reason

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
+//! This is a font glyph caching and consolidation layer for games. It's part
+//! of the Psilo engine, but it doesn't depend on any other part of the engine.
+//! Given some TTF outline fonts, and renderer-specific code for the actual
+//! mechanics of the atlases, this layer provides multichannel signed distance
+//! field generation and atlas packing. It does not precalculate the atlases,
+//! instead populating them at runtime whenever glyphs are actually used. This
+//! provides moderately worse packing efficiency, in exchange for increased
+//! flexibility when rendering unexpected glyphs.
+//!
+//! Signed distance fields are a clever way to get decent-quality realtime text
+//! rendering with low runtime cost. Multichannel signed distance fields, as
+//! originated by Viktor Chlumský, are an even more clever way to greatly
+//! improve the quality of the rendering with only a trivial increase in
+//! rendering overhead and even a potential *reduction* in video memory
+//! overhead. For more information, see [Chlumský's Master's thesis][1].
+//!
+//! [1]: https://github.com/Chlumsky/msdfgen/files/3050967/thesis.pdf
+
 use std::{
     collections::HashMap,
     rc::Rc,
